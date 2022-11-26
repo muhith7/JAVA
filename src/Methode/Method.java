81d1a929405b3b70f3c3976470662fb7b0425582
muhith7 <abdulmuhithnawawi@gmail.com>
2022-11-26 12:42:21 +0700

    abdul

---
+++ src/Methode/Method.java
@@ -0,0 +1,34 @@
+package Methode;
+
+public class Method {
+    //Method 3
+    // non void , tanda parameter
+    public static int[] inputAngka(){
+        int [] number = new int[]{1,2,3,4,5};
+        return number;
+    }
+    public static String _Message(){
+
+        return null;
+    }
+
+    //contoh
+    public static String _Muhith(){
+        return "Hello Method 3";//tidak ada void maka harus ada yg dikembalikan
+    }
+
+    public static void main(String[] args) {
+        String msg=_Muhith(); // untuk menanggil method
+        System.out.println(msg); // untuk memanggil return
+
+        int[] angkaSaya = inputAngka();
+        for (int n : angkaSaya) {           //for ringkas
+            System.out.println("angka saya =" + n);
+        }
+
+        for (int i=0; i<angkaSaya.length; i++){ //for biasa
+            System.out.println("menggunakan for biasa = " + angkaSaya[i]);
+        }
+    }
+
+}
